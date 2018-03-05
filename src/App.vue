f3be5d436deb382e5036f07473f63662288cf7eb
jiaoqiqi <1176932165@qq.com>
2018-03-05 20:46:02 +0800

    hello vue
    Signed-off-by: jiaoqiqi <1176932165@qq.com>

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,36 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <router-view/>
+    <hello></hello>
+    <!--<router-view/>-->
   </div>
 </template>
 
 <script>
+
+import Hello from './components/Hello'
+
 export default {
-  name: 'App'
+  name: 'app',
+  components: {
+    Hello
+  }
 }
 </script>
 
+<!--<script>-->
+<!--export default {-->
+<!--name: 'App'-->
+<!--}-->
+<!--</script>-->
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <h1>{{msg}}</h1>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'hello'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
